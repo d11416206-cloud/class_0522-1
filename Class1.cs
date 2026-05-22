9d1b6ed752acc9cdd3d96ead2975d3142328b33a
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-05-22 11:33:47 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class IData
+{
+	public string GetName();
+
+}
+
+public interface ICard
+{
+    public string GetCard();
+}
+
+public interface IName : IData, ICard
+{
+    public string GetName();
+}
+
+public class Person : IName
+{
+    public string GetName()
+    {
+        return "Name";
+    }
+    public string GetCard()
+    {
+        return "Card";
+    }
+    public string GetData()
+    {
+        return "Data";
+    }
+}
+public class Student : ICard
+{
+
+}
+    
+    
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace class_0522_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
